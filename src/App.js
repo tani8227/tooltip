1d634eb4ca3e02b0abe4b4b8294c4e105c51f6d5
Tarun <tarunmalviya8827@gmail.com>
2024-11-05 14:38:41 +0530

    tooltip completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,104 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [toolTip, setToolTip] = useState('');
+  const [btnLeftColor, setBtnLeftColor] = useState();
+  const [btnRightColor, setBtnRightColor] = useState();
+  const [btnTopColor, setBtnTopColor] = useState();
+  const [btnDownColor, setBtnDownColor] = useState();
+  const [mouse, setMouse] = useState(false);
+
+  function toolTipLeftSetter(msg) {
+    setToolTip(msg);
+    setBtnLeftColor('red');
+    setBtnRightColor('');
+    setBtnTopColor('');
+    setBtnDownColor('');
+  }
+
+  function toolTipRightSetter(msg) {
+    setToolTip(msg);
+    setBtnLeftColor('');
+    setBtnRightColor('red');
+    setBtnTopColor('');
+    setBtnDownColor('');
+  }
+
+  function toolTipTopSetter(msg) {
+    setToolTip(msg);
+    setBtnLeftColor('');
+    setBtnRightColor('');
+    setBtnTopColor('red');
+    setBtnDownColor('');
+  }
+
+  function toolTipDownSetter(msg) {
+    setToolTip(msg);
+    setBtnLeftColor('');
+    setBtnRightColor('');
+    setBtnTopColor('');
+    setBtnDownColor('red');
+  }
+
+  
+  function check() {
+    setMouse(true);
+  }
+
+  function recheck() {
+    setTimeout(() => setMouse(false), 200); 
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-evenly', alignItems: 'center', width: '100%', height: '300px' }}>
+      <h3>Select the Position Below !!! </h3>
+      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', padding: 5 }}>
+        <button style={{ backgroundColor: btnLeftColor }} onClick={() => toolTipLeftSetter('Left')}>
+          Left
+        </button>
+        <button style={{ backgroundColor: btnRightColor }} onClick={() => toolTipRightSetter('Right')}>
+          Right
+        </button>
+        <button style={{ backgroundColor: btnTopColor }} onClick={() => toolTipTopSetter('Top')}>
+          Top
+        </button>
+        <button style={{ backgroundColor: btnDownColor }} onClick={() => toolTipDownSetter('Down')}>
+          Down
+        </button>
+      </div>
+
+      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%', position: 'relative' }}>
+        <div
+          onMouseOver={check}
+          onMouseOut={recheck}
+          style={{ backgroundColor: 'pink', padding: '5px 10px', borderRadius: '4px' }}
+          className="hoverme"
         >
-          Learn React
-        </a>
-      </header>
+          Hover Over me
+        </div>
+        
+        {mouse && (
+          <div
+            style={{
+              width: '50%',
+              position: 'absolute',
+              pointerEvents: 'none', 
+              textAlign: 'center',
+              backgroundColor: 'black',
+              color: 'white',
+             
+             
+              ...(toolTip === 'Left' && { left: '-100px', top: '0' }),
+              ...(toolTip === 'Right' && { right: '-100px', top: '0' }),
+              ...(toolTip === 'Top' && { bottom: '50px', left: '25%', }),
+              ...(toolTip === 'Down' && { top: '50px', right: '25%',  }),
+              ...(toolTip === '' && { bottom: '50px'  }),
+            }}
+          >
+            {toolTip ? `Thanks for hovering ${toolTip} Position` : 'Thanks for hovering! I am a ToolTip'}
+          </div>
+        )}
+      </div>
     </div>
   );
 }
